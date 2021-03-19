8e5008c6891a4ab14595ecb663aff948d5371212
Siyi <63399966+Irislah@users.noreply.github.com>
2021-03-19 16:10:18 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <!-- <NewListing></NewListing> -->
+  </div>
+</template>
+
+<script>
+//import NewListing from './components/NewListing.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    //NewListing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <img id="bg" src="../assets/login.jpg">
+        
+  </div>
+</template>
+
+<script>
+
+export default ({
+    data() {
+        return {
+
+        }
+    },
+    components:{
+
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
